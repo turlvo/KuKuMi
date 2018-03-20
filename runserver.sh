8832f84e97d8cfc8cbac431d19d746368494d756
KuKu <turlvo@gmail.com>
2018-03-20 14:34:26 +0900

    changing running daemon command from script to systemd

--- runserver.sh
+++ runserver.sh
@@ -5,13 +5,6 @@ cd /root/KuKuMi
 
 echo ""
 
-echo "Running XiaomiBT daemon..."
-cd /root/KuKuMi/xiaomibt-daemon
-python xiaomibt-daemon.py stop
-python xiaomibt-daemon.py start
-
-echo ""
-
 echo "Running KuKu Mi Server..."
 cd /root/KuKuMi/django-KuKuMi
 python manage.py runserver 0:8484
