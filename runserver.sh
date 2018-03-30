408978aee0ab3410ae982b6b4e824ff6285f1eb1
KuKu <turlvo@gmail.com>
2018-03-30 22:31:15 +0900

    update script

--- runserver.sh
+++ runserver.sh
@@ -4,8 +4,12 @@ cd /root/KuKuMi
 ./update.sh
 
 echo ""
+cd /root/KuKuMi/xiaomibt-daemon
+python xiaomibt-daemon.py stop
 
 echo "Running KuKu Mi Server..."
 cd /root/KuKuMi/django-gentelella
+python manage.py makemigrations
+python manage.py migrate
 python manage.py runserver 0:8484
 
