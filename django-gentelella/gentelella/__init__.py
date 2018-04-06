31a4514429ea96a1fa92ae56a89784ee32a32d59
KuKu <turlvo@gmail.com>
2018-04-06 21:05:44 +0900

    added daemon start and stop routine when server is started and stopped

---
+++ django-gentelella/gentelella/__init__.py
@@ -0,0 +1,17 @@
+import subprocess
+import signal
+import sys
+
+def startup():
+    print ("Startup...")
+    p = subprocess.Popen(["python", "../xiaomibt-daemon/xiaomibt-daemon.py", "restart"])
+
+def signal_handler(signal, frame):
+        print('You pressed Ctrl+C!')
+        p = subprocess.Popen(["python", "../xiaomibt-daemon/xiaomibt-daemon.py", "stop"])
+        sys.exit(0)
+
+startup()
+signal.signal(signal.SIGINT, signal_handler)
+signal.pause()
+
