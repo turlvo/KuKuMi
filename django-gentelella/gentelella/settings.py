467689c1a5a72f8e0c550a4a1808e1f0e379e51c
KuKu <turlvo@gmail.com>
2019-01-12 23:07:35 +0900

    changed db location to '/config'

--- django-gentelella/gentelella/settings.py
+++ django-gentelella/gentelella/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'gentelella.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('\config', 'db.sqlite3'),
     }
 }
 
