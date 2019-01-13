69fcdb0be784b45b3b6a3f773a33ab7a4fa8bb83
KuKu <turlvo@gmail.com>
2019-01-13 11:11:35 +0900

    remove update routine in runserver.sh

--- runserver.sh
+++ runserver.sh
@@ -1,18 +1,8 @@
 #!/bin/sh
-echo "Updating KuKuMi..."
-cd /root/KuKuMi
-git checkout -f
-git pull
-
-pip install --upgrade -r requirements.txt
-
-echo ""
 cd /root/KuKuMi/xiaomibt-daemon
 python xiaomibt-daemon.py stop
 
 echo "Running KuKu Mi Server..."
 cd /root/KuKuMi/django-gentelella
-python manage.py makemigrations
-python manage.py migrate
 python manage.py runserver 0:8484
 
